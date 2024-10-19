27ca8d8c5a51cf49fe30f39d1f2bb4245faa9423
ZershaMuzamal <zershamuzamal@gmail.com>
2024-10-19 21:02:11 +0400

    # SRING CONCATINATION word1 = 'welcome' word2 = 'to' word3 ='bathspa'  print(word1,word2,word3)
    # SRING CONCATINATION
    word1 = 'welcome'
    word2 = 'to'
    word3 ='bathspa'
    
    print(word1,word2,word3)

---
+++ EXERCISE 01/Coding is cool.py
@@ -0,0 +1,17 @@
+"""## Exercise 1: Coding is Cool- 10 Marks
+Fill in the blanks in the Python code below to output the phrase **"Coding is Cool"** to the console using variables and string concatenation.
+### Fill in the blanks below
+```python
+word1 = 
+word2 = 
+word3 =
+# Use string concatenation to combine the variables and print the phrase
+_____(_________)
+_________________________________________________________________________________"""
+
+# SRING CONCATINATION
+word1 = 'welcome'
+word2 = 'to'
+word3 ='bathspa'
+
+print(word1,word2,word3)
