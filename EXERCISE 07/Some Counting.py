0b3b8fae16ff271620a28308d0494fc4b5cdff66
ZershaMuzamal <zershamuzamal@gmail.com>
2024-11-10 23:54:28 +0400

    Some Counting.py

---
+++ EXERCISE 07/Some Counting.py
@@ -0,0 +1,59 @@
+"""## Exercise 7: Some Counting - 20 Marks
+Use your newly acquired knowledge of the for loop to complete the following tasks. Print all values to the console
+in each case.
+* Write a loop that counts up from 0 to 50 in increments of 1.
+* Write a loop that counts down from 50 to 0 in decrements of 1.
+* Write a loop that counts up from 30 to 50 in increments of 1.
+* Write a loop that counts down from 50 to 10 in decrements of 2.
+* Write a loop that counts up from 100 to 200 in increments of 5.
+*You may include all loops in a single project*"""
+
+#LOOP THAT COUNTS TO FROM 0 TO 50 IN INCREMENT OF 1
+
+#PRINTING THE HEADING OF THE LOOP AND MAKING IT APPEAR IN THE CENTER
+print("LOOP THAT COUNTS UP FROM 0 TO 50 IN INCREMENT OF 1".center(80,'-'))
+#ADDING FOR LOOP
+for a in range(0 ,51 , 1):
+ print(a)
+ #ADDING EXTRA PRINT STATEMENT FOR BETTER VISIBILITY
+ print()
+
+#LOOP THAT COUNTS DOWN FROM 50 TO 0 IN DECREMENT OF 1
+
+#PRINTING THE HEADING OF THE LOOP AND MAKING IT APPEAR IN THE CENTER
+print("LOOP THAT COUNTS DOWN FROM 50 TO 0 IN DECREMENT OF 1".center(80,'-'))
+#ADDING FOR LOOP
+for b in range(50, -1, -1):
+ print(b)
+ #ADDING EXTRA PRINT STATEMENT FOR BETTER VISIBILITY
+ print()
+
+#LOOP THAT COUNTS UP FROM 30 TO 50 IN INCREMENT OF 1
+
+#PRINTING THE HEADING OF THE LOOP AND MAKING IT APPEAR IN THE CENTER
+print("LOOP THAT COUNTS UP FROM 30 TO 50 IN INCREMENT OF 1".center(80,'-'))
+#ADDING FOR LOOP
+for c in range(30, 51, 1):
+ print(c)
+ #ADDING EXTRA PRINT STATEMENT FOR BETTER VISIBILITY
+ print()
+
+#LOOP THAT COUNTS DOWN FROM 50 TO 10 IN DECREMENT OF 2
+
+#PRINTING THE HEADING OF THE LOOP AND MAKING IT APPEAR IN THE CENTER
+print("LOOP THAT COUNTS DOWN FROM 50 TO 10 IN DECREMENT OF 2".center(80,'-'))
+#ADDING FOR LOOP
+for d in range (50, 9, -2):
+ print(d)
+ #ADDING EXTRA PRINT STATEMENT FOR BETTER VISIBILITY
+ print()
+ 
+#LOOP THAT COUNTS UP FROM 100 TO 200 WITH INCREMENT OF 5
+
+#PRINTING THE HEADING OF THE LOOP AND MAKING IT APPEAR IN THE CENTER
+print("LOOP THAT COUNTS UP FROM 100 TO 200 WITH INCREMENT OF 5".center(80,'-'))
+#ADDING FOR LOOP
+for e in range (100, 201 ,5):
+ print(e)
+ #ADDING EXTRA PRINT STATEMENT FOR BETTER VISIBILITY
+ print()
