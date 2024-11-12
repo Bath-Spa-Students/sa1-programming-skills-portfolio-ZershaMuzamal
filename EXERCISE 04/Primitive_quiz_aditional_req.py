cb856386b2b9dad33c5ae8e7078be621af019f6b
ZershaMuzamal <zershamuzamal@gmail.com>
2024-11-13 01:13:44 +0400

    Primitive_quiz_aditional_req.py

---
+++ EXERCISE 04/Primitive_quiz_aditional_req.py
@@ -0,0 +1,136 @@
+## Exercise 4: Primitive Quiz - 30 Marks
+"""In this exercise, you'll create a simple program that asks the user a question, evaluates their answer,
+and provides feedback."""
+### Steps:
+"""1. Write a program that asks the user "What is the capital of France?" and waits for their response.
+2. If the user's answer is correct (i.e., "Paris"), the program should print a message saying the answer
+is correct.
+3. If the answer is incorrect, the program should print a message saying the answer is wrong."""
+###Advanced Requirements:
+"""Ignore Capitalization: Modify your program to accept answers regardless of the capitalization
+(e.g., "paris", "Paris", and "PaRis" 
+should all be considered correct).
+Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries.
+ Provide feedback for each question."""
+
+#PROGRAMING FOR ADDITIONAL REQUIREMENTS with if-else STATEMENT
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 1
+print("What is the capital of France?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "paris":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 2
+print("What is the capital of Albania?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Tirana":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 3
+print("What is the capital of Austria?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Vienna":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 4
+print("What is the capital of Belgium?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Brussels":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 5
+print("What is the capital of Bulgaria?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Sofia":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 6
+print("What is the capital of Finland?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Helsinki":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 7
+print("What is the capital of Georgia?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Tbilisi":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 8 
+print("What is the capital of Germany?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Berlin":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 9 
+print("What is the capital of Italy?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Rome":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
+
+#USING PRINT STATEMENT TO PRINT THE QUESTION 10
+print("What is the capital of Portugal?")
+#TAKING ANSWER AS INPUT FROM THE USER
+ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+if ANSWER.lower() == "Lisbon":
+#PRINT STATEMENT FOR CORRECT ANSWER
+   print("Correct!")
+else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+   print("Wrong")
