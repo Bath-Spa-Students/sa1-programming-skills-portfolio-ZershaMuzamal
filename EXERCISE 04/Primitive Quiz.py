4fc10367df4336f77305591146922a852c325a96
ZershaMuzamal <zershamuzamal@gmail.com>
2024-11-20 02:37:51 +0400

    Primitive Quiz.py

--- EXERCISE 04/Primitive Quiz.py
+++ EXERCISE 04/Primitive Quiz.py
@@ -1,11 +1,24 @@
 """## Exercise 4: Primitive Quiz - 30 Marks
 In this exercise, you'll create a simple program that asks the user a question, evaluates their answer, and provides feedback.
-
 ### Steps:
 1. Write a program that asks the user "What is the capital of France?" and waits for their response.
 2. If the user's answer is correct (i.e., "Paris"), the program should print a message saying the answer is correct.
 3. If the answer is incorrect, the program should print a message saying the answer is wrong.
-
-### Advanced Requirements:
-Ignore Capitalization: Modify your program to accept answers regardless of the capitalization (e.g., "paris", "Paris", and "PaRis" should all be considered correct).
-Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries. Provide feedback for each question."""
+###Advanced Requirements:
+Ignore Capitalization: Modify your program to accept answers regardless of the capitalization (e.g., "paris", "Paris", and "PaRis" 
+should all be considered correct).
+Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries. Provide feedback for each
+question."""
+#USING PRINT STATEMENT TO PRINT THE QUESTION
+print("What is the capital of France?")
+while True:
+#TAKING ANSWER AS INPUT FROM THE USER
+   ANSWER = input("Answer here:")
+#USING IF AND ELSE STAEMENT 
+   if ANSWER.lower() == "paris":
+#PRINT STATEMENT FOR CORRECT ANSWER
+      print("Correct! WOW YOU WON")
+      break
+   else:
+#PRINT STATEMENT FOR WRONG ANSWER  
+      print("Wrong!! LET'S TRY AGAIN.")
