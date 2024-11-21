2c21e4d5176349f7ab0810f11c7fe55873b57e8c
ZershaMuzamal <zershamuzamal@gmail.com>
2024-11-21 09:45:57 +0400

    biography.py

--- EXERCISE 03/biography.py
+++ EXERCISE 03/biography.py
@@ -3,7 +3,7 @@
 dictionary."""
 ### Steps:
 """1. Store the information (name, hometown, and age) as key-value pairs in a dictionary.
-2. Print the values on separate lines using a single `print()` statement.
+2. Print the values on separate lines using a single print() statement.
 3. Use variables with appropriate data types for each piece of information."""
 ### Advanced Requirements:
 """Have the user input their name, hometown, and age instead of hardcoding the values.
@@ -11,16 +11,17 @@ Try giving both your first and second name when asked for your name. What happen
 How can you handle multiple words in Python?
 Test the program by entering a string value for age (e.g., "twenty"). What happens?
 How can you prevent this issue?"""
-#ADING THE BIOGRAPHY DICTIONARY
+# ADDING THE BIOGRAPHY DICTIONARY
 Biography = {
-# ADDING NAME
-'name' : 'ZERSHA' ,
-#ADDING HOMETOWN
-'hometown' : 'Fujairah',
-#ADDING AGE
-'age' : '19'
+    # ADDING NAME
+    'name' : 'ZERSHA',
+    # ADDING HOMETOWN
+    'hometown' : 'Fujairah',
+    # ADDING AGE
+    'age' : '19'
 }
+
 #PRINTING VALUES WITH CONCATINATION AND MAKING SURE ALL THE VALUES ARE STRINGS
-print('name: ' + Biography['name'] + "\n" +
-'hometown: ' + Biography['hometown'] + "\n" +
-'age: ' + str(Biography['age']))
+print(f'name: ' + str(Biography['name']) + "\n" +
+      'hometown: ' + str(Biography['hometown']) + "\n" +
+      'age: ' + str(Biography['age']))
